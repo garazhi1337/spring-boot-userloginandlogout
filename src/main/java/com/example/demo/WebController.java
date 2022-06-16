7afda1f34e5ecb0cb8a977515aa8d0158053536d
arturuser2006 <artkrukov2006@gmail.com>
2022-06-16 12:59:27 +0300

    Add existing project files to Git

---
+++ src/main/java/com/example/demo/WebController.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.repositories.User;
+import com.example.demo.repositories.UserRepository;
+
+@Controller
+public class WebController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/")
+	public String getHome(Model model) {
+		return "home";
+	}
+	
+	@GetMapping("/registration")
+	public String getRegistration(Model model) {
+		return "registration";
+	}
+	
+	@PostMapping("/registration")
+	public String addNewUser (@RequestParam String email, @RequestParam String password) {
+
+	    User user = new User(password, email);
+	    userRepository.save(user);
+	    return "redirect:/";
+	}
+	
+}
---
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+
+}
